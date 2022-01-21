0fd7834c79606f5a80afd9fdc52046de12fa8d0b
Signe Syreeta <sisyreet@ga-g1.msk.21-school.ru>
2022-01-21 16:07:42 +0300

    improved zoom, alt

--- fdf/sources/main.c
+++ fdf/sources/main.c
@@ -6,7 +6,7 @@
 /*   By: sisyreet <sisyreet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 15:20:30 by sisyreet          #+#    #+#             */
-/*   Updated: 2022/01/21 15:22:46 by sisyreet         ###   ########.fr       */
+/*   Updated: 2022/01/21 16:06:47 by sisyreet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ int	key_hook(int key, t_data *data)
 	if (key == 53)
 		exit(0);
 	if (key == 69)
-		data->zoom += 15;
+		data->zoom += 5;
 	if (key == 78)
 	{
 		if (data->zoom > 15)
-			data->zoom -= 15;
+			data->zoom -= 5;
 	}
 	if (key == 123)
 		data->shift_x += 75;
@@ -52,7 +52,7 @@ int	main(int argc, char **argv)
 	{
 		data = (t_data *)malloc(sizeof(data));
 		screen.x = 2400;
-		screen.y = 1300;
+		screen.y = 1080;
 		get_map(data, argv[1]);
 		data->mlx_ptr = mlx_init();
 		data->win_ptr = mlx_new_window(data->mlx_ptr, screen.x, \
@@ -60,7 +60,7 @@ int	main(int argc, char **argv)
 		data->color = 0x03fc07;
 		data->zoom = 30;
 		data->shift_x = screen.x / 2;
-		data->shift_y = -10;
+		data->shift_y = 0;
 		draw(data, data->points);
 		mlx_key_hook(data->win_ptr, key_hook, data);
 		mlx_loop(data->mlx_ptr);
