fa7049c24c84c6f4b4c9d9df3b2a93e32932b356
sisyreet <91873904+sisyreet@users.noreply.github.com>
2022-02-12 21:07:43 +0300

    Binary search function

--- myfunks/binary_search.c
+++ myfunks/binary_search.c
@@ -4,12 +4,14 @@
 
 int	binary_search(int *arr, int length, int item)
 {
-	int low = 0;
-	int	high = length - 1;
+	int	low;
+	int	high;
 	int mid;
 	int guess;
 	int count;
 
+	low = 0;
+	high = length - 1;
 	count = 0;
 	while (low <= high)
 	{
@@ -27,19 +29,29 @@ int	binary_search(int *arr, int length, int item)
 	return (mid);
 }
 
-int main(int argc, char **argv)
+int main(void)
 {
-	int arr[argc - 1];
+	int	*array;
+	long long length;
 	int	i;
 	int	num;
 
-	printf("Type number and I find it very quickly:\n");
-	scanf("%d", &num);
-	i = 0;
-	while (i < argc - 1)
+	printf("Type quantity of elements in array:\n");
+	scanf("%lld", &length);
+
+	array = (int *)malloc(sizeof(int) * length);
+	if (!array)
 	{
-		arr[i] = atoi(argv[i + 1]);
-		i++;
+		printf("This array too large!\n");
+		exit(-1);
 	}
-	binary_search(arr, argc - 1, num);
+	i = 0;
+	while(i < length)
+	{
+		array[i] = i + 1;
+		i++; 
+	}
+	printf("Type number and I'll find it very quickly:\n");
+	scanf("%d", &num);
+	binary_search(array, length, num);
 }
